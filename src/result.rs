3465372284f6f4bb98cfe6cd41c9cf887cdc8d28
Jeremy Fitzhardinge <jsgf@fb.com>
2017-01-31 13:24:26 -0800

    Implement Display and Error for pom::Error

--- src/result.rs
+++ src/result.rs
@@ -1,3 +1,6 @@
+use std::fmt::{self, Display};
+use std::error;
+
 /// Parser error.
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -5,6 +8,29 @@ pub enum Error {
 	Mismatch { message: String, position: usize },
 	Conversion { message: String, position: usize },
 	Custom { message: String, position: usize, inner: Option<Box<Error>> },
+}
+
+impl error::Error for Error {
+	fn description(&self) -> &'static str {
+		"Parse error"
+	}
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			&Error::Incomplete =>
+				write!(f, "Incomplete"),
+			&Error::Mismatch { ref message, ref position } =>
+				write!(f, "Mismatch at {}: {}", position, message),
+			&Error::Conversion { ref message, ref position } =>
+				write!(f, "Conversion failed at {}: {}", position, message),
+			&Error::Custom { ref message, ref position, inner: Some(ref inner) } =>
+				write!(f, "Custom error at {}: {} (inner: {})", position, message, inner),
+			&Error::Custom { ref message, ref position, inner: None } =>
+				write!(f, "Custom error at {}: {}", position, message),
+		}
+	}
 }
 
 /// Parser result, `Result<O>` ia alias of `Result<O, pom::Error>`.
