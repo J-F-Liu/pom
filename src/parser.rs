197f98c5dba8b297c50b14736a00faad8d1d23b2
Blake Hawkins <blake.hawkins.11@gmail.com>
2019-07-09 23:50:54 +0100

    improve example
    >> get(0) -> map

--- src/parser.rs
+++ src/parser.rs
@@ -508,7 +508,7 @@ impl<'a, I: Copy + 'a, O: 'a, U: 'a> Mul<Parser<'a, I, U>> for Parser<'a, I, O> 
 	}
 }
 
-/// Chain two passers where the second parser depends on the first's result.
+/// Chain two parsers where the second parser depends on the first's result.
 impl<'a, I: Copy, O: 'a, U: 'a, F: Fn(O) -> Parser<'a, I, U> + 'a> Shr<F> for Parser<'a, I, O> {
 	type Output = Parser<'a, I, U>;
 
