e2cd303065e09e2f08d11b7c0b4b344ddea74bbf
Nigel Eke <nigeleke@gmail.com>
2024-03-04 09:06:24 +1100

    Add RangeInclusive and RangeToInclusive to Sets

--- src/set.rs
+++ src/set.rs
@@ -1,6 +1,6 @@
 use std::{
 	cmp::{PartialEq, PartialOrd},
-	ops::{Range, RangeFrom, RangeFull, RangeTo},
+	ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive},
 	str,
 };
 
@@ -43,9 +43,21 @@ impl<T: PartialOrd + Copy> Set<T> for RangeFrom<T> {
 	}
 }
 
+impl<T: PartialOrd + Copy> Set<T> for RangeInclusive<T> {
+	fn contains(&self, elem: &T) -> bool {
+		self.start() <= elem && self.end() >= elem
+	}
+}
+
 impl<T: PartialOrd + Copy> Set<T> for RangeTo<T> {
 	fn contains(&self, elem: &T) -> bool {
 		self.end > *elem
+	}
+}
+
+impl<T: PartialOrd + Copy> Set<T> for RangeToInclusive<T> {
+	fn contains(&self, elem: &T) -> bool {
+		self.end >= *elem
 	}
 }
 
@@ -67,4 +79,51 @@ impl<const N: usize> Set<u8> for [u8; N] {
 	fn to_str(&self) -> &str {
 		str::from_utf8(self).unwrap_or("<byte array>")
 	}
+}
+
+#[cfg(test)]
+mod test {
+	use crate::parser::*;
+
+	#[test]
+	fn one_of_using_set() {
+		assert!(one_of(b"az").parse(b"a").is_ok());
+		assert!(one_of(b"az").parse(b"1").is_err());
+	}
+
+	#[test]
+	fn one_of_using_range() {
+		assert!(one_of(&(b'a'..b'z')).parse(b"a").is_ok());
+		assert!(one_of(&(b'a'..b'z')).parse(b"z").is_err());
+		assert!(one_of(&(b'a'..b'z')).parse(b"1").is_err());
+	}
+
+	#[test]
+	fn one_of_using_range_to() {
+		assert!(one_of(&(..b'z')).parse(b"a").is_ok());
+		assert!(one_of(&(..b'z')).parse(b"z").is_err());
+		assert!(one_of(&(..b'z')).parse(b"1").is_ok());
+	}
+
+	#[test]
+	fn one_of_using_range_inclusive() {
+		assert!(one_of(&(b'a'..=b'z')).parse(b"a").is_ok());
+		assert!(one_of(&(b'a'..=b'z')).parse(b"z").is_ok());
+		assert!(one_of(&(b'a'..=b'z')).parse(b"1").is_err());
+	}
+
+	#[test]
+	fn one_of_using_range_to_inclusive() {
+		assert!(one_of(&(..=b'z')).parse(b"a").is_ok());
+		assert!(one_of(&(..=b'z')).parse(b"z").is_ok());
+		assert!(one_of(&(..=b'z')).parse(b"1").is_ok());
+	}
+
+	#[test]
+	fn one_of_using_full_range() {
+		assert!(one_of(&(..)).parse(b"a").is_ok());
+		assert!(one_of(&(..)).parse(b"z").is_ok());
+		assert!(one_of(&(..)).parse(b"1").is_ok());
+	}
+
 }
